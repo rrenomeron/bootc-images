bb70bb0f876d776d98e5a950e0210b139d6c3e10
Rich Renomeron <rrenomeron+github@gmail.com>
2025-06-19 14:38:16 -0400

    refactor: gschema overrides
    - explicit override for enabling the logo menu
    - rename main TR UI override to clarify its purpose

--- files/scripts/enable-logo-menu.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-echo "Adding Logo Menu to default enabled extensions" 
-tee /etc/dconf/db/distro.d/10-tr-enabled-extensions << EOF
-[org/gnome/shell]
-enabled-extensions=['appindicatorsupport@rgcjonas.gmail.com','dash-to-dock@micxgx.gmail.com','blur-my-shell@aunetx','system-monitor-next@paradoxxx.zero.gmail.com', 'accent-directories@taiwbi.com']
-EOF 
-
-echo "Done"
