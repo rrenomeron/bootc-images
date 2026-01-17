292b86abb55a859c7a2984f76938f08e0d90fd1a
Rich Renomeron <rrenomeron+github@gmail.com>
2026-01-16 21:00:30 -0500

    fix: add minimal motd for alma/silverblue
    Needed becuase the switch to OCI for bluefin goodies required a working ublue-motd setup

---
+++ files/scripts/generate-motd-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+set -eoux pipefail
+
+mkdir -p /usr/share/ublue-os/motd
+cat > /usr/share/ublue-os/motd/env.sh << EOF
+#!/usr/bin/env sh
+# KEEP THIS SMALL
+# This will run on every shell that a user starts up.
+MOTD_IMAGE_NAME=$IMAGE_NAME
+MOTD_IMAGE_TAGE=$OS_VERSION
+EOF
---
+++ files/ublue-motd/template.md
@@ -0,0 +1,3 @@
+This bootc based system is running
+${MOTD_IMAGE_NAME}:${MOTD_IMAGE_TAG}
+
